6da8534d92a858c63b6a6addb58ce363a583275b
konserva93 <itsergeev93@gmail.com>
2020-11-20 10:49:29 +0400

    styling

--- src/Layout.js
+++ src/Layout.js
@@ -6,6 +6,13 @@ const svgs = reqSvgs
   .keys()
   .map(path => ({ path, file: reqSvgs(path) }));
 
+const Header = styled.div`
+  text-align: center;
+  font-size: 36px;
+  color: #EBE5E6;
+  margin-bottom: 48px;
+`;
+
 const Grid = styled.div`
   display: grid;
   display: -ms-grid;
@@ -14,14 +21,16 @@ const Grid = styled.div`
   grid-template-rows: repeat(${props => props.rows}, 150px);
   -ms-grid-rows: (150px)[${props => props.rows}];
   width: 100%;
-  height: 100%;
+  background-color: #F1F7ED;
+  max-width: 1138px;
+  margin: 0 auto;
 `;
 
 const Item = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: ${props => props.color};
+  background: ${props => props.background};
   width: 100%;
   height: 100%;
   -ms-grid-row: ${props => props.row + 1};
@@ -33,23 +42,31 @@ const Image = styled.img`
   height: ${props => props.size || '100%'};
 `;
 
+const backgrounds = {
+  transparent: 'rgba(0,0,0,0)',
+  blue: 'linear-gradient(rgba(76, 102, 99, 0) 5%, rgba(76, 102, 99, .3), rgba(76, 102, 99, 0) 95%)',
+}
+
 export function Layout(props) {
   return (
-    <Grid columns={props.columns} rows={Math.floor(svgs.length / props.columns) + (svgs.length % props.columns)}>
-      {svgs.map((item, index) => {
-        return <Item
-          key={index}
-          row={Math.floor(index / props.columns)}
-          column={index % props.columns}
-          color={index % 2 === 0 ? '#fff' : '#cdcdcd'}
-        >
-          <Image
-            src={item.file.default}
-            className="test"
-            size={Math.floor(index / props.columns) % 2 === 0 ? '75%' : '100%'}
-          />
-        </Item>;
-      })}
-    </Grid>
+    <>
+      <Header>{'Наши партнеры'}</Header>
+      <Grid columns={props.columns} rows={Math.floor(svgs.length / props.columns) + (svgs.length % props.columns)}>
+        {svgs.map((item, index) => {
+          return <Item
+            key={index}
+            row={Math.floor(index / props.columns)}
+            column={index % props.columns}
+            background={[1, 2].includes(Math.floor(index / props.columns)) ? '#C5CBC2' : backgrounds.transparent}
+          >
+            <Image
+              src={item.file.default}
+              className="test"
+              size={Math.floor(index / props.columns) % 2 === 0 ? '75%' : '100%'}
+            />
+          </Item>;
+        })}
+      </Grid>
+    </>
   );
 }
